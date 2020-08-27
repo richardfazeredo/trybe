6f1effaa617fbd92984af7d21a617ab6988cffca
richardfazeredo <richardcompletetour@gmail.com>
2020-08-27 17:32:35 -0300

    Praticando posicionamento com css

---
+++ exercices/4_1/operacoes-aritmeticas.js
@@ -0,0 +1,16 @@
+let a = 14, b = 7;
+
+//Adição
+console.log(a + b);
+
+//Subtração
+console.log(a - b);
+
+//Multiplicação
+console.log(a * b);
+
+//Divisão
+console.log(a / b);
+
+//Módulo
+console.log(a % b);
