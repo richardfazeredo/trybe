905a0e9fd839e57a2fc2fad7ce82af27a2e04277
richardfazeredo <richardcompletetour@gmail.com>
2020-08-27 17:46:17 -0300

    Operações aritméticas - Exercício 1

---
+++ exercices/4_1/operacoes-aritmeticas.js
@@ -0,0 +1,16 @@
+let a = 14, b = 7;
+
+//Operação com Adição
+console.log(a + b);
+
+//Operação com Subtração
+console.log(a - b);
+
+//Operação com Multiplicação
+console.log(a * b);
+
+//Operação com Divisão
+console.log(a / b);
+
+//Operação com Módulo
+console.log(a % b);
