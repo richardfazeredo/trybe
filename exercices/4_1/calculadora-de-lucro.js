eb8eb02d238a9d773fc559795eba81a93c2219f4
richardfazeredo <richardcompletetour@gmail.com>
2020-08-28 16:10:45 -0300

    Programa que calcula o lucro de um produto após 1000 vendas - exercicio 10

---
+++ exercices/4_1/calculadora-de-lucro.js
@@ -0,0 +1,6 @@
+let valDeCusto, valDeVenda;
+
+let quantVendas = 1000;
+let lucroTotal = (valDeVenda - (valDeCusto *= 1.2)) * quantVendas;
+
+return `O lucro total das vendas foi de R$ ${lucroTotal}`;
